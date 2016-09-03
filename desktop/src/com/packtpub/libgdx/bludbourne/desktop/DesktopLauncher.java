ded030b499bf9ef1b3a263d19f1058485d6170bc
niko neuhauser <niko.neuhauser@gmail.com>
2016-09-03 16:49:13 +0200

    Finished chapter 3.

--- desktop/src/com/packtpub/libgdx/bludbourne/desktop/DesktopLauncher.java
+++ desktop/src/com/packtpub/libgdx/bludbourne/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.packtpub.libgdx.bludbourne.desktop;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.packtpub.libgdx.bludbourne.BludBourne;
@@ -7,6 +8,14 @@ import com.packtpub.libgdx.bludbourne.BludBourne;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new BludBourne(), config);
+
+		config.height = 600;
+		config.width = 800;
+		config.title = "BludBourne";
+		config.useGL30 = false;
+
+		Application app = new LwjglApplication(new BludBourne(), config);
+		app.setLogLevel(Application.LOG_DEBUG);
+		app.setLogLevel(Application.LOG_INFO);
 	}
 }
